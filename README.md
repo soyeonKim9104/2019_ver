3e52c89d42ac2a9a207d0b879d01df115abf6b08
soyeonKim9104 <soyean7979@gmail.com>
2019-07-02 16:29:15 +0900

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+# soyeon2019
+2019-04-02
+2019 soyeon portfolio project
+
---
+++ js/custom.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 4,
+        spaceBetween: 50,
+        // init: false,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+            }
+        }
+    });
+});
