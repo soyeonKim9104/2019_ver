8ecc3349ee6acef3431a3f33d44dbb236f6c7748
soyeonKim9104 <soyean7979@gmail.com>
2019-07-03 09:46:09 +0900

    2019.07.02

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,56 @@
+"use strict";
+
 $(document).ready(function(){
-    var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 4,
-        spaceBetween: 50,
-        // init: false,
-        pagination: {
-            el: '.swiper-pagination',
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-            1024: {
-                slidesPerView: 4,
-                spaceBetween: 40,
+    /*custom js start*/
+    var ksy2019 = function($){
+        var common = {
+            init: function () {
+                common.customSwiper();
+                common.customSelect();
             },
-            768: {
-                slidesPerView: 3,
-                spaceBetween: 30,
+            customSwiper: function () {
+                var swiper = new Swiper('.swiper-container', {
+                    slidesPerView: 4,
+                    spaceBetween: 50,
+                    // init: false,
+                    pagination: {
+                        el: '.swiper-pagination',
+                    },
+                    navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                    },
+                    breakpoints: {
+                        1024: {
+                            slidesPerView: 4,
+                            spaceBetween: 40,
+                        },
+                        768: {
+                            slidesPerView: 3,
+                            spaceBetween: 30,
+                        },
+                        640: {
+                            slidesPerView: 2,
+                            spaceBetween: 20,
+                        },
+                        320: {
+                            slidesPerView: 1,
+                            spaceBetween: 10,
+                        }
+                    }
+                });
             },
-            640: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-            },
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 10,
+            customSelect : function() {
+
             }
-        }
+        };
+        return common;
+    }(jQuery);
+
+    $(function($){
+        ksy2019.init();
     });
+
 });
+
+
